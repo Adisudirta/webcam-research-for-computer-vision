90b45bb4beb732897ef27cbded0dd0907dc91dbd
Adisudirta <yanadisudirta@gmail.com>
2024-08-14 14:54:36 +0800

    feat: finalize web rtc implementation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import '../app.pcss';
+
+	import { Toaster } from '$lib/components/ui/sonner';
 </script>
+
+<svelte:head>
+	<title>Webcam Research by Adi</title>
+</svelte:head>
+
+<Toaster position="top-center" />
 
 <div class="container">
 	<slot />
--- src/routes/manual/+page.svelte
+++ src/routes/manual/+page.svelte
@@ -1,26 +1,121 @@
 <script lang="ts">
-	import Button from '$lib/components/ui/button/button.svelte';
+	import { toast } from 'svelte-sonner';
+	import { onDestroy, onMount } from 'svelte';
+
 	import * as Select from '$lib/components/ui/select';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Compressor from 'compressorjs';
 
 	let isCameraStarted: boolean = false;
 	let cameraDeviceList: MediaDeviceInfo[] = [];
+	let selectedCameraDevice: {
+		label: string;
+		value: string;
+	};
+	let video: HTMLVideoElement;
+	let canvas: HTMLCanvasElement;
+	let stream: MediaStream | null = null;
 
 	async function getDevices() {
 		if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
-			console.log('enumerated devices not supported');
+			toast.error('Enumerated devices not supported');
 			return false;
 		}
 
-		await navigator.mediaDevices.getUserMedia({ video: true });
+		try {
+			await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
 
-		cameraDeviceList = await navigator.mediaDevices.enumerateDevices();
+			const mediaDevices = await navigator.mediaDevices.enumerateDevices();
+			cameraDeviceList = mediaDevices.filter((device) => device.kind === 'videoinput');
+
+			selectedCameraDevice = {
+				label: cameraDeviceList[0].label,
+				value: cameraDeviceList[0].deviceId
+			};
+		} catch {
+			toast.error('Camera not found');
+			return false;
+		}
 	}
 
-	async function start() {
+	function dataURLtoFile(dataUrl: string, filename: string) {
+		var arr = dataUrl.split(','),
+			mime = arr[0].match(/:(.*?);/)?.[1] || '',
+			bstr = atob(arr[arr.length - 1]),
+			n = bstr.length,
+			u8arr = new Uint8Array(n);
+		while (n--) {
+			u8arr[n] = bstr.charCodeAt(n);
+		}
+		return new File([u8arr], filename, { type: mime });
+	}
+
+	function getSnapshot() {
+		if (video && canvas) {
+			canvas.width = video.videoWidth;
+			canvas.height = video.videoHeight;
+
+			canvas.getContext('2d')?.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+			const fileUrl = canvas.toDataURL('image/jpeg', 0.6);
+			const file = dataURLtoFile(fileUrl, 'snapshot.jpg');
+
+			console.log('Size before compress', file.size);
+
+			new Compressor(file, {
+				quality: 0.2,
+
+				success(result: File | Blob) {
+					console.log('Size after compress', result.size);
+
+					// Download the compressed file
+					const downloadLink = document.createElement('a');
+					downloadLink.href = URL.createObjectURL(result);
+					downloadLink.download = 'compressed.jpg';
+					downloadLink.click();
+				},
+				error(err) {
+					toast.error(err.message);
+				}
+			});
+		}
+	}
+
+	function stopCamera() {
+		if (stream && video) {
+			const tracks = stream.getTracks();
+			tracks.forEach((track) => track.stop());
+			video.srcObject = null;
+			video.removeAttribute('src');
+			video.load();
+		}
+	}
+
+	async function openCamera(deviceId: string) {
+		stopCamera();
+
+		try {
+			stream = await navigator.mediaDevices.getUserMedia({
+				video: {
+					deviceId
+				}
+			});
+			video.srcObject = stream;
+		} catch (e) {
+			console.log('error', e);
+			toast.error('Camera not found');
+			return false;
+		}
+	}
+
+	onMount(async () => {
 		isCameraStarted = true;
-
 		await getDevices();
-	}
+	});
+
+	onDestroy(() => {
+		stopCamera();
+	});
 </script>
 
 <div class="container">
@@ -29,10 +124,25 @@
 	<hr />
 
 	<div class="my-5 flex w-full items-center justify-center space-x-4">
-		<Button on:click={start} class="w-fit">Start Camera</Button>
+		<Button on:click={async () => openCamera(selectedCameraDevice.value)} class="w-fit">
+			Start Camera
+		</Button>
 
-		{#if isCameraStarted && cameraDeviceList.length}
-			<Select.Root>
+		<Button disabled={!isCameraStarted} on:click={stopCamera} variant="destructive" class="w-fit">
+			Stop Camera
+		</Button>
+
+		<Button disabled={!isCameraStarted} on:click={getSnapshot} variant="outline" class="w-fit">
+			Snapshot
+		</Button>
+
+		{#if cameraDeviceList.length}
+			<Select.Root
+				selected={selectedCameraDevice}
+				onSelectedChange={() => {
+					openCamera(selectedCameraDevice.value);
+				}}
+			>
 				<Select.Trigger class="w-[300px]">
 					<Select.Value placeholder="Select camera devices" />
 				</Select.Trigger>
@@ -55,7 +165,7 @@
 			<h3 class="text-center text-2xl font-semibold text-black">Video Camera</h3>
 
 			{#if isCameraStarted}
-				<video id="video" playsinline autoplay>
+				<video bind:this={video} playsinline autoplay>
 					<track kind="captions" />
 				</video>
 			{/if}
@@ -63,7 +173,8 @@
 
 		<div>
 			<h3 class="text-center text-2xl font-semibold text-black">Snapshot</h3>
-			<canvas id="canvas"></canvas>
+
+			<canvas bind:this={canvas} />
 		</div>
 	</div>
 </div>
